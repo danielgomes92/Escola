8b298309fd03d3c384323703fefbbdc0d1479071
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 17:48:29 -0300

    modificado - informações para persistir e consultar no sql

--- EscolaSQL/Aluno.cs
+++ EscolaSQL/Aluno.cs
@@ -12,6 +12,7 @@ namespace Escola
 {
     public class Aluno
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Matricula { get; set; }
         public List<double> Notas { get; set; }
@@ -59,8 +60,24 @@ namespace Escola
                         while (dr.Read()) // enquanto estiver lendo vai ser transformado na lista de aluno
                         {
                             var aluno = new Aluno();
+                            aluno.Id = Convert.ToInt32(dr["id"]);
                             aluno.Nome = dr["nome"].ToString();
                             aluno.Matricula = dr["matricula"].ToString();
+
+                            
+                            using (var cmdNotas = new SqlCommand("select * from notas where aluno_id=" + aluno.Id, cnn)) // cmd = Command
+                            {
+                                using (SqlDataReader drNotas = cmdNotas.ExecuteReader()) // dr = Data Reader
+                                {
+                                    aluno.Notas = new List<double>();
+                                    while (drNotas.Read()) // enquanto estiver lendo vai ser transformado na lista de aluno
+                                    {
+                                        aluno.Notas.Add(Convert.ToDouble(drNotas["nota"]));
+
+                                    }
+                                }
+                            }
+                            
                         }
                     }
                 }
@@ -85,6 +102,25 @@ namespace Escola
             Aluno.alunos.Add(aluno);
             string caminho = @"E:\PersistenciadeDados\alunos.json";
             File.WriteAllText(Aluno.CaminhoJson(), JsonConvert.SerializeObject(Aluno.alunos));
+        }
+
+        public static void AdicionarSql(Aluno aluno)
+        {
+            using (var cnn = new SqlConnection(Aluno.stringConexaoSql()))
+            {
+                var cmd = new SqlCommand("insert into alunos(nome, matricula) values (@nome, @matricula);select @@identity", cnn);
+                cmd.Parameters.AddWithValue("@nome", aluno.Nome);
+                cmd.Parameters.AddWithValue("@matricula", aluno.Matricula);
+                int aluno_id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                foreach (var nota in aluno.Notas)
+                {
+                    var cmdNota = new SqlCommand("insert into notas(aluno_id, nota) values (@aluno_id, @nota)", cnn);
+                    cmd.Parameters.AddWithValue("@aluno_id", aluno_id);
+                    cmd.Parameters.AddWithValue("@nota", nota);
+                    cmdNota.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
