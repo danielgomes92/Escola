d8e005679a869d92d3c28be7f182a6b635782fae
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-10 16:55:10 -0300

    projeto escola com persistencia de arquivo físico JSON

---
+++ Aluno.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Escola
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public List<double> Notas { get; set; }
+
+        public double Media()
+        {
+            double somaNotas = 0;
+            foreach (var nota in this.Notas)
+                somaNotas += nota;
+
+            return somaNotas / this.Notas.Count;
+        }
+
+        public string Situacao()
+        {
+            return (this.Media() > 6 ? "Aprovado" : "Reprovado");
+        }
+
+        public string NotasFormadata()
+        {
+            return string.Join(",", this.Notas);
+        }
+
+        private static List<Aluno> alunos = new List<Aluno>();
+        public static List<Aluno> Todos()
+        {
+            if (File.Exists(Aluno.CaminhoJson()))
+            {
+                var conteudo = File.ReadAllText(Aluno.CaminhoJson());
+                Aluno.alunos = JsonConvert.DeserializeObject<List<Aluno>>(conteudo);
+            }
+
+            return Aluno.alunos;
+        }
+
+        private static string CaminhoJson()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["caminho_json"];
+        }
+
+        public static void Adicionar(Aluno aluno)
+        {
+            Aluno.alunos = Aluno.Todos();
+            Aluno.alunos.Add(aluno);
+            string caminho = @"E:\PersistenciadeDados\alunos.json";
+            File.WriteAllText(Aluno.CaminhoJson(), JsonConvert.SerializeObject(Aluno.alunos));
+        }
+    }
+}
